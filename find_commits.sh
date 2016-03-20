1e440d8c2ce4d78613d9b650e12e061ef1860467
Elazar Gershuni <elazarg@gmail.com>
2016-03-20 04:44:27 +0200

    minor docs

--- find_commits.sh
+++ find_commits.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-function join {
+function join_delimited {
+ # The Art Of Copying And Pasting From Stackoverflow:
  local IFS="$1"; shift; echo "$*"; 
 }
 
 function find_projects {
+ # we look for something like CASSANDRA-1234 with enough occurrences
+ # it's only heuristical though.
  git log --pretty=oneline | egrep -oe"[A-Z]{3,15}[ -][[:digit:]]{3,5}" | sed 's/[ -].*//g' \
  | sort | uniq -c | grep -v '\s[0-9] ' | cut -f 5 -d' '
 }
@@ -23,5 +26,5 @@ WORKDIR=${1:-.}
 cd $WORKDIR
 
 PROJECTS=$(find_projects)
-commit_to_issuekey "($(join '|' $PROJECTS))"
-echo "Projects found: $(join ' ' $PROJECTS)" | grep --color '.*' > /dev/stderr
+commit_to_issuekey "($(join_delimited '|' $PROJECTS))"
+echo "Projects found: $(join_delimited ' ' $PROJECTS)" | grep --color '.*' > /dev/stderr
